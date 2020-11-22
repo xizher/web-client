4acd36077710d70408e91f7cc7308d11ded4936c
xizher <xizher@163.com>
2020-11-22 16:23:49 +0800

    feat: 数据库后台管理系统

--- portal-v2/src/components/base/bs-ui/index.ts
+++
@@ -1,9 +0,0 @@
-import BsButton from './button/bs-button.vue'
-import BsInput from './input/bs-input.vue'
-import BsNavbar from './navbar/bs-navbar.vue'
-
-export {
-  BsButton,
-  BsInput,
-  BsNavbar,
-}
