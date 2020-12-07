8411eaa21fc5a9401ca6e9b0bb314acccdce8813
xizher <xizher@163.com>
2020-12-07 19:14:18 +0800

    feat: *

--- vue.config.js
+++ vue.config.js
@@ -3,24 +3,28 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const productionGzipExtensions = ['js', 'css']
 const cdn = {
   css: [
-    'https://unpkg.com/ant-design-vue@2.0.0-rc.2/dist/antd.min.css'
+    './libs/ant-design-vue/antd.min.css',
+    './libs/openlayers/ol.css',
+    './libs/leaflet/leaflet.css',
   ],
   js: [
-    'https://unpkg.com/vue@3.0.0/dist/vue.global.js',
-    'https://unpkg.com/vue-router@4.0.0-rc.6/dist/vue-router.global.js',
-    'https://unpkg.com/vuex@4.0.0-rc.2/dist/vuex.global.js',
-    'https://unpkg.com/axios@0.21.0/dist/axios.min.js',
-    'https://unpkg.com/ant-design-vue@2.0.0-rc.2/dist/antd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/enc-base64.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/md5.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/evpkdf.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/cipher-core.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/aes.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/pad-pkcs7.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/mode-ecb.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/enc-utf8.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/enc-hex.min.js',
+    './libs/vue/vue.global.js',
+    './libs/vue/vue-router.global.js',
+    './libs/vue/vuex.global.js',
+    './libs/axios/axios.min.js',
+    './libs/ant-design-vue/antd.min.js',
+    './libs/crypto/core.min.js',
+    './libs/crypto/enc-base64.min.js',
+    './libs/crypto/md5.min.js',
+    './libs/crypto/evpkdf.min.js',
+    './libs/crypto/cipher-core.min.js',
+    './libs/crypto/aes.min.js',
+    './libs/crypto/pad-pkcs7.min.js',
+    './libs/crypto/mode-ecb.min.js',
+    './libs/crypto/enc-utf8.min.js',
+    './libs/crypto/enc-hex.min.js',
+    './libs/openlayers/ol.js',
+    './libs/leaflet/leaflet.js',
   ]
 }
 
@@ -49,7 +53,10 @@ module.exports = {
       axios: 'axios',
       'ant-design-vue': 'antd',
       'ant-design-vue/dist/antd.css': 'antd',
-      'crypto-js': 'CryptoJS'
+      'crypto-js': 'CryptoJS',
+      'ol': 'ol',
+      'leaflet': 'L',
+      'leaflet/dist/leaflet.css': 'L',
     }
   },
   lintOnSave: false,
