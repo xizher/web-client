3055ccaab4fab57fa43e8cdb3893fb55202ea919
xizher <xizher@163.com>
2020-12-16 11:58:31 +0800

    feat: 系统界面样式

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,49 @@
         <div class="vertical-bar" />
       </div>
     </div>
-    <div
-
-      class="map-view-container"
-    >
-      <div id="map-view-container" />
-      <div class="map-top-left-bar map-bar" />
-      <div class="map-top-right-bar map-bar">
-        <!-- <button>
+    <div class="content">
+      <div
+        v-if="sideVisible"
+        class="side"
+      >
+        <div class="side-title" />
+        <div class="side-content">
+          <a-select
+            style="width: 80%;"
+          >
+            <a-select-option value="jack">
+              自然地理环境的地域差异
+            </a-select-option>
+            <a-select-option value="lucy">
+              城市与城市化
+            </a-select-option>
+            <a-select-option value="Yiminghe">
+              区域生态环境建设
+            </a-select-option>
+            <a-select-option value="Yiminghe2">
+              区域自然资源的综合开发与利用
+            </a-select-option>
+          </a-select>
+        </div>
+      </div>
+      <div class="main">
+        <div class="map-panel">
+          <div id="map-view-container" />
+          <div class="map-top-left-bar map-bar" />
+          <div class="map-top-right-bar map-bar">
+            <!-- <button>
           test
         </button> -->
+          </div>
+          <div class="map-bottom-left-bar map-bar" />
+          <div class="map-bottom-right-bar map-bar" />
+          <div
+            class="side-panel-switch"
+            :class="sideVisible ? 'side-panel-switch__opend' : 'side-panel-switch__closed'"
+            @click="sideVisible = !sideVisible"
+          />
+        </div>
       </div>
-      <div class="map-bottom-left-bar map-bar" />
-      <div class="map-bottom-right-bar map-bar" />
     </div>
     <!-- <div class="mouse-location-info">
       lon: {{ lon }}, lat: {{ lat }} | x: {{ x }}, y: {{ y }}
@@ -27,7 +57,7 @@
 </template>
 
 <script>
-import { onMounted, toRefs } from 'vue'
+import { onMounted, ref, toRefs } from 'vue'
 import { WebMap } from './map/mapinit/mapinit'
 import { webMapOptions } from './config/app.conf'
 export default {
@@ -37,12 +67,15 @@ export default {
     const webMap = new WebMap('map-view-container', webMapOptions)
     const { mouseLocation } = webMap.useHooks()
 
+    const sideVisible = ref(false)
+
     onMounted(() => {
       webMap.init()
     })
 
     return {
-      ...toRefs(mouseLocation)
+      ...toRefs(mouseLocation),
+      sideVisible
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import './assets/styles/layout/layout.min.css'
 import './assets/styles/overwrite/overwrite.min.css'
 import { loadArcGISJsApiModules } from './map'
 
 loadArcGISJsApiModules().then(() => {
   createApp(App)
+    .use(Antd)
     .mount('#app')
 })
