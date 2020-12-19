0713d409a84034d5395d326159f77bc4fc2e7133
xizher <xizher@163.com>
2020-12-19 21:03:47 +0800

    feat: sth. map

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'ant-design-vue/dist/antd.css'
 import './assets/styles/layout/layout.scss'
 import './assets/styles/module/module.scss'
 import './assets/styles/overwrite/overwrite.scss'
+import './ext/js.ext'
 import { loadEsriModules, loadEsriExt } from './map/esri'
 
 loadEsriModules().then(() => {
